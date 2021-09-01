dfcbda2ad3e405466462ba7471da8a7f26dd47b4
zpp0516 <1723451602@qq.com>
2021-09-01 19:49:28 +0800

    spring5-webflux

---
+++ webfluxdemo2/src/test/java/com/example/webfluxdemo2/Webfluxdemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webfluxdemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
