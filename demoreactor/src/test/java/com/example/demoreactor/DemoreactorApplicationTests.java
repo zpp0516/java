96bc090b9b6ad6adf467de13c2394c266b1a297f
zpp0516 <1723451602@qq.com>
2021-09-01 19:37:09 +0800

    spring5中的webflux

---
+++ demoreactor/src/test/java/com/example/demoreactor/DemoreactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoreactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoreactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
